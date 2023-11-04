186ec289501c90e66cea07f49f4ce2ab536f81f9
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2023-11-05 00:51:42 +0100

    cards( accountsCount, revenue, serviceRequests, vendorsCount) done + lineChart (needs fixing)

--- app/src/main/java/com/deksi/bpsfmmobileapp/home/fragments/HomeFragment.kt
+++ app/src/main/java/com/deksi/bpsfmmobileapp/home/fragments/HomeFragment.kt
@@ -53,44 +53,70 @@ class HomeFragment : Fragment() {
 
 
     private fun dataLoading() {
-            val sharedPreferences =
-                requireActivity().getSharedPreferences("AuthToken", Context.MODE_PRIVATE)
-            val authToken = sharedPreferences.getString("transferObject", null)
+        val sharedPreferences =
+            requireActivity().getSharedPreferences("AuthToken", Context.MODE_PRIVATE)
+        val authToken = sharedPreferences.getString("transferObject", null)
 
-            val headers = HashMap<String, String>()
-            authToken?.let {
-                headers["Authorization"] = "Bearer $it"
+        val headers = HashMap<String, String>()
+        authToken?.let {
+            headers["Authorization"] = "Bearer $it"
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://bps-fms-staging.azurewebsites.net/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val dashboardApiService = retrofit.create(DashboardApiService::class.java)
+        val requestDashboard = DashboardRequest(16, "11-09-2023", "12-10-2023")
+
+
+        val call = dashboardApiService.getDashboardData(requestDashboard, Headers.of(headers))
+        call.enqueue(object : Callback<DashboardResponse> {
+            override fun onResponse(
+                call: Call<DashboardResponse>,
+                response: Response<DashboardResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val dashboardResponse = response.body()
+
+                    cards(dashboardResponse)
+                    sliderCards(dashboardResponse)
+                    totalConsumptionSeparatedTabGraph(dashboardResponse)
+                    lineGraphs(dashboardResponse)
+
+                }
             }
 
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://bps-fms-staging.azurewebsites.net/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+            override fun onFailure(call: Call<DashboardResponse>, t: Throwable) {
+                Log.e("Error", "Failed to make the API request", t)
+            }
+        })
+    }
 
-            val dashboardApiService = retrofit.create(DashboardApiService::class.java)
-            val requestDashboard = DashboardRequest(16, "11-09-2023", "12-10-2023")
+    private fun cards(dashboardResponse: DashboardResponse?) {
 
+        /// *** accountsCount card ***
+        val textViewAccountsCount = binding.textViewAccountsCount
+        val accountsCount = dashboardResponse?.transferObject?.accountsCount
+        textViewAccountsCount.text = accountsCount.toString()
 
-            val call = dashboardApiService.getDashboardData(requestDashboard, Headers.of(headers))
-            call.enqueue(object : Callback<DashboardResponse> {
-                override fun onResponse(
-                    call: Call<DashboardResponse>,
-                    response: Response<DashboardResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        val dashboardResponse = response.body()
+        /// *** revenue card ***
+        val textViewRevenue = binding.textViewRevenue
+        val revenue = dashboardResponse?.transferObject?.revenue
+        textViewRevenue.text = revenue.toString()
 
-                        sliderCards(dashboardResponse)
-                        totalConsumptionSeparatedTabGraph(dashboardResponse)
+        /// *** serviceRequestsCount card ***
+        val textViewServiceRequests = binding.textViewServiceRequests
+        val serviceRequestsCount = dashboardResponse?.transferObject?.serviceRequestsCount
+        textViewServiceRequests.text = serviceRequestsCount.toString()
 
-                    }
-                }
+        /// *** vendorsCount card ***
+        val textViewVendors = binding.textViewVendors
+        val vendorsCount = dashboardResponse?.transferObject?.vendorsCount
+        textViewVendors.text = vendorsCount.toString()
+    }
 
-                override fun onFailure(call: Call<DashboardResponse>, t: Throwable) {
-                    Log.e("Error", "Failed to make the API request", t)
-                }
-            })
-        }
 
     private fun sliderCards(dashboardResponse: DashboardResponse?) {
 
@@ -104,7 +130,8 @@ class HomeFragment : Fragment() {
         textViewPreviousValuePowerCuts.text = previousValuePowerCuts.toString()
 
         val textViewPercentageDiffPowerCuts = binding.textViewPercentageDiffPowerCuts
-        val percentageDiffPowerCuts = dashboardResponse?.transferObject?.powerCuts?.percentageDiff.toString()
+        val percentageDiffPowerCuts =
+            dashboardResponse?.transferObject?.powerCuts?.percentageDiff.toString()
         val formattedPercentageDiffPowerCuts = "$percentageDiffPowerCuts%"
         textViewPercentageDiffPowerCuts.text = formattedPercentageDiffPowerCuts
 
@@ -115,43 +142,49 @@ class HomeFragment : Fragment() {
         textViewCurrentValuePowerCutsNum.text = currentValuePowerCutsNum.toString()
 
         val textViewPreviousValuePowerCutsNum = binding.textViewPreviousValuePowerCutsNum
-        val previousValuePowerCutsNum = dashboardResponse?.transferObject?.powerCutsNum?.previousValue
+        val previousValuePowerCutsNum =
+            dashboardResponse?.transferObject?.powerCutsNum?.previousValue
         textViewPreviousValuePowerCutsNum.text = previousValuePowerCutsNum.toString()
 
         val textViewPercentageDiffPowerCutsNum = binding.textViewPercentageDiffPowerCutsNum
-        val percentageDiffPowerCutsNum = dashboardResponse?.transferObject?.powerCutsNum?.percentageDiff.toString()
+        val percentageDiffPowerCutsNum =
+            dashboardResponse?.transferObject?.powerCutsNum?.percentageDiff.toString()
         val formattedPercentageDiffPowerCutsNum = "$percentageDiffPowerCutsNum%"
         textViewPercentageDiffPowerCutsNum.text = formattedPercentageDiffPowerCutsNum
 
 
-
         /// *** energySavings card ***
         val textViewCurrentValueEnergySavings = binding.textViewCurrentValueEnergySavings
-        val currentValueEnergySavings = dashboardResponse?.transferObject?.energySavings?.currentValue
+        val currentValueEnergySavings =
+            dashboardResponse?.transferObject?.energySavings?.currentValue
         textViewCurrentValueEnergySavings.text = currentValueEnergySavings.toString()
 
         val textViewPreviousValueEnergySavings = binding.textViewPreviousValueEnergySavings
-        val previousValueEnergySavings = dashboardResponse?.transferObject?.energySavings?.previousValue
+        val previousValueEnergySavings =
+            dashboardResponse?.transferObject?.energySavings?.previousValue
         textViewPreviousValueEnergySavings.text = previousValueEnergySavings.toString()
 
         val textViewPercentageDiffEnergySavings = binding.textViewPercentageDiffEnergySavings
-        val percentageDiffEnergySavings = dashboardResponse?.transferObject?.energySavings?.percentageDiff.toString()
+        val percentageDiffEnergySavings =
+            dashboardResponse?.transferObject?.energySavings?.percentageDiff.toString()
         val formattedPercentageDiffEnergySavings = "$percentageDiffEnergySavings%"
         textViewPercentageDiffEnergySavings.text = formattedPercentageDiffEnergySavings
 
 
-
         /// *** carbonSavings card ***
         val textViewCurrentValueCarbonSavings = binding.textViewCurrentValueCarbonSavings
-        val currentValueCarbonSavings = dashboardResponse?.transferObject?.carbonSavings?.currentValue
+        val currentValueCarbonSavings =
+            dashboardResponse?.transferObject?.carbonSavings?.currentValue
         textViewCurrentValueCarbonSavings.text = currentValueCarbonSavings.toString()
 
         val textViewPreviousValueCarbonSavings = binding.textViewPreviousValueCarbonSavings
-        val previousValueCarbonSavings = dashboardResponse?.transferObject?.carbonSavings?.previousValue
+        val previousValueCarbonSavings =
+            dashboardResponse?.transferObject?.carbonSavings?.previousValue
         textViewPreviousValueCarbonSavings.text = previousValueCarbonSavings.toString()
 
         val textViewPercentageDiffCarbonSavings = binding.textViewPercentageDiffCarbonSavings
-        val percentageDiffCarbonSavings = dashboardResponse?.transferObject?.energySavings?.percentageDiff.toString()
+        val percentageDiffCarbonSavings =
+            dashboardResponse?.transferObject?.energySavings?.percentageDiff.toString()
         val formattedPercentageDiffCarbonSavings = "$percentageDiffCarbonSavings%"
         textViewPercentageDiffCarbonSavings.text = formattedPercentageDiffCarbonSavings
 
@@ -166,11 +199,39 @@ class HomeFragment : Fragment() {
 
         val entries = mutableListOf<BarEntry>()
 
-        val customLabelsX = arrayOf("Apr 1", "Apr 2", "Apr 3", "Apr 4",
-            "Apr 5", "Apr 6", "Apr 7", "Apr 8", "Apr 9", "Apr 10", "Apr 11", "Apr 12", "Apr 13", "Apr 14",
-            "Apr 15", "Apr 16", "Apr 17", "Apr 18", "Apr 19", "Apr 20", "Apr 21", "Apr 22", "Apr 23",
-            "Apr 24", "Apr 25", "Apr 26", "Apr 27", "Apr 28", "Apr 29", "Apr 30", "Apr 31"
-            )
+        val customLabelsX = arrayOf(
+            "Apr 1",
+            "Apr 2",
+            "Apr 3",
+            "Apr 4",
+            "Apr 5",
+            "Apr 6",
+            "Apr 7",
+            "Apr 8",
+            "Apr 9",
+            "Apr 10",
+            "Apr 11",
+            "Apr 12",
+            "Apr 13",
+            "Apr 14",
+            "Apr 15",
+            "Apr 16",
+            "Apr 17",
+            "Apr 18",
+            "Apr 19",
+            "Apr 20",
+            "Apr 21",
+            "Apr 22",
+            "Apr 23",
+            "Apr 24",
+            "Apr 25",
+            "Apr 26",
+            "Apr 27",
+            "Apr 28",
+            "Apr 29",
+            "Apr 30",
+            "Apr 31"
+        )
 
         for (i in 0 until (gridTotalConsumption?.size ?: 0)) {
             val energy = gridTotalConsumption?.get(i)?.energy ?: 0f
@@ -184,7 +245,6 @@ class HomeFragment : Fragment() {
 
         val barData = BarData(dataSet)
         dataSet.color = Color.BLUE
-
 
 
         val xAxis = barChart.xAxis
@@ -209,7 +269,80 @@ class HomeFragment : Fragment() {
         barChart.data = barData
         barChart.invalidate()
 
+    }
+
+
+    private fun lineGraphs(dashboardResponse: DashboardResponse?) {
+
+        // needs fixing
+        val actual = dashboardResponse?.transferObject?.energySources
+        val received = dashboardResponse?.transferObject?.received
+        val expectedSolar = dashboardResponse?.transferObject?.totalConsumptionSeparated?.solarTotalConsumption
+
+        val lineChart = binding.lineChartReceivedExpectedActual
+
+
+        val customLabelsX = arrayOf(
+            "Sep 1", "Sep 3", "Sep 6", "Sep 9",
+            "Sep 12", "Sep 15", "Sep 18", "Sep 21", "Sep 24", "Sep 27", "Sep 30"
+        )
+
+        val entriesActual = actual?.mapIndexed { index, data ->
+            data.energy?.let { Entry(index.toFloat(), it.toFloat()) }
         }
+
+        val entriesReceived = received?.mapIndexed { index, data ->
+            data.energy?.let { Entry(index.toFloat(), it.toFloat()) }
+        }
+
+        val entriesExpectedSolar = expectedSolar?.mapIndexed { index, data ->
+            data.energy?.let { Entry(index.toFloat(), it) }
+        }
+
+        val dataSetActual = LineDataSet(entriesActual, "Actual")
+        dataSetActual.color = Color.parseColor("#F79009")
+        dataSetActual.setCircleColor(Color.parseColor("#F79009"))
+        dataSetActual.setDrawValues(false)
+
+        val dataSetReceived = LineDataSet(entriesReceived, "Received")
+        dataSetReceived.color = Color.parseColor("#05AA6C")
+        dataSetReceived.setCircleColor(Color.parseColor("#05AA6C"))
+        dataSetReceived.setDrawValues(false)
+
+        val dataSetExpected = LineDataSet(entriesExpectedSolar, "Expected")
+        dataSetExpected.color = Color.parseColor("#FD5C63")
+        dataSetExpected.setCircleColor(Color.parseColor("#FD5C63"))
+        dataSetExpected.setDrawValues(false)
+
+
+        val xAxis = lineChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(customLabelsX)
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.labelCount = customLabelsX.size
+        xAxis.granularity = 1f
+
+        val leftYAxis = lineChart.axisLeft
+        val rightYAxis = lineChart.axisRight
+
+        leftYAxis.setDrawLabels(false)
+        leftYAxis.setDrawGridLines(false)
+
+        rightYAxis.setDrawLabels(false)
+        rightYAxis.setDrawGridLines(false)
+
+
+        val lineData = LineData(dataSetActual, dataSetReceived, dataSetExpected)
+
+        lineChart.data = lineData
+
+
+        lineChart.legend.isEnabled = false
+        lineChart.description.isEnabled = false
+        lineChart.invalidate()
+
+    }
 
 
 }
+
+
